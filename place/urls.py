4cf5756c5940e57a250f4dadb85fc89518327413
elcipeng <tamajustin322@gmail.com>
2025-10-26 23:29:44 +0700

    menyelesaikan modul place!!

--- place/urls.py
+++ place/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.csrf import csrf_exempt
 
 app_name = 'place'
 
@@ -12,11 +13,11 @@ urlpatterns = [
     path("add/", views.add_place, name="add_place"),
     path('<int:pk>/', views.place_detail, name='place_detail'),
     path('<int:place_id>/', views.place_detail, name='place_detail'),
-    path('add_review/<int:pk>/', views.add_review, name='add_review'),
+    path('<int:pk>/add_review/', csrf_exempt(views.add_review), name='add_review'),
+    path('review/<int:review_id>/delete/', csrf_exempt(views.delete_review), name='delete_review'),
     path('<int:pk>/edit/', views.edit_place, name='edit_place'),
     path('<int:pk>/delete/', views.delete_place, name='delete_place'),
     path('<int:pk>/delete-image/', views.delete_image, name='delete_image'),
-    path('review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
     # URL untuk trigger loading data
     path('load/cycling/', views.load_places_cycling, name='load_cycling'),
     path('load/running/', views.load_places_running, name='load_running'),
